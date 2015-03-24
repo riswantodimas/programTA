eefb8c2045f77cad60e77c6f5b485d0189ef0521
riswantodimas <riswantodimas@aol.com>
2015-03-24 10:36:12 +0700

    moving graph

--- liveupdate.py
+++ liveupdate.py
@@ -4,18 +4,44 @@ import numpy as np
 import time
 
 fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
+ax1 = plt.subplot(1,1,1)
+
 xar = []
 yar = []
-n=0
+for q in range(20,0,-1):
+    xar.insert(q,q)
+    yar.insert(q,0)
 xinit=time.time()
 
-def animate(i):
+
+"""def appendgraph(i):
     yar.append(np.random.randint(20))
     xar.append(time.time()-xinit)
     ax1.clear()
     ax1.plot(xar,yar)
-ani = animation.FuncAnimation(fig, animate, interval=910)
+    ax1.set_autoscaley_on(False)
+    plt.ylim((0,20))
+    print(xar)
+    print(yar)"""
+
+def movegraph(i):
+    for n in range(19,0,-1):
+        yar.insert(n,yar.pop(n-1))
+        xar.insert(n,xar.pop(n-1))
+    yar[0]=np.random.randint(20)
+    xar[0]=time.time()-xinit
+    #xar.append(time.time()-xinit)
+    ax1.clear()
+    ax1.plot(xar,yar)
+    ax1.set_autoscaley_on(False)
+    plt.ylim((0,20))
+    plt.xlim([min(xar),max(xar)])
+    print(xar)
+
+ani = animation.FuncAnimation(fig, movegraph, interval=910)
+
 plt.show()
+
+
 
 #hohohoho
